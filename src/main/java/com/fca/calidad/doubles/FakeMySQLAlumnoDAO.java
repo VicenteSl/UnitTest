a39cbaa274dd23ae486c177c7a1064e5aa5bf247
VicenteSl <64109031+VicenteSl@users.noreply.github.com>
2022-05-08 14:37:59 -0500

    Add files via upload

---
+++ src/main/java/com/fca/calidad/doubles/FakeMySQLAlumnoDAO.java
@@ -0,0 +1,29 @@
+package com.fca.calidad.doubles;
+
+public class FakeMySQLAlumnoDAO implements IAlumnoDAO {
+
+	
+	public boolean addAlumno(Alumno a) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	
+	public boolean deleteAlumno(Alumno a) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	
+	public boolean updateEmail(Alumno a) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	
+	public Alumno consultarAlumno(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
